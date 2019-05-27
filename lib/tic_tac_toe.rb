dbbbca4fd6389ad263759f9bba1b02e6325efbcd
Abagail Hudson <abagailanne@gmail.com>
2019-05-27 21:38:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,18 @@
 class TicTacToe
+
+  WIN_COMBINATIONS = []
+
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+  
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
@@ -17,5 +31,9 @@ class TicTacToe
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index
+    
   end
 end
